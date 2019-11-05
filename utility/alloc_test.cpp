cdc94b77669ee7b6e4109cce81aa889f8242037e
chengfu <chengfu.wcy@antfin.com>
2019-11-05 14:07:29 +0800

    Remove info

--- utility/alloc_test.cpp
+++ utility/alloc_test.cpp
@@ -16,13 +16,13 @@ struct Mallocator {
     Mallocator(const Mallocator<U>&) noexcept {}
 
     T* allocate(std::size_t n) {
-        std::cout<< "WENCHUNYANG::allocate" << std::endl;
+        std::cout<< "User defined::allocate" << std::endl;
         if(n > std::size_t(-1) / sizeof(T)) throw std::bad_alloc();
         if(auto p = static_cast<T*>(std::malloc(n*sizeof(T)))) return p;
         throw std::bad_alloc();
     }
     void deallocate(T* p, std::size_t) noexcept {
-        std::cout<< "WENCHUNYANG::deallocate" << std::endl;
+        std::cout<< "User defined::deallocate" << std::endl;
         std::free(p); }
 };
 template <class T, class U>
