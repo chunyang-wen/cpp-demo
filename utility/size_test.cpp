4d516f854b82db36c983f38527800e8a81c54a01
chengfu <chengfu.wcy@antfin.com>
2020-07-06 11:47:38 +0800

    add size_test

---
+++ utility/size_test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <type_traits>
+
+using namespace std;
+
+
+template<int n>
+class Pointer {
+    public:
+    typedef int Type;
+};
+
+template<>
+class Pointer<8> {
+    public:
+    typedef long Type;
+};
+
+template<>
+class Pointer<4> {
+    public:
+    typedef int Type;
+};
+
+
+int main() {
+    int* a;
+    cout << sizeof(Pointer<sizeof(a)>::Type) << endl;
+}
+
